0eb517c2191461bff4a1a298f9ec16efb87cb8c7
Josh Espinoza <joshespi@gmail.com>
2024-12-09 11:37:07 -0700

    refactor: clean up sidebar PHP code by removing commented-out shortcodes

--- wp-content/themes/crest-child/sidebar-about.php
+++ wp-content/themes/crest-child/sidebar-about.php
@@ -1,7 +1,6 @@
 <aside id="mainSidebar">
 	<h1>School Information</h1>
-	<?php 
-	// echo do_shortcode( '[schoolinfo]' );
+	<?php
 	$schoolinfo_menu = get_post(117);
 	echo do_shortcode($schoolinfo_menu->post_content);
 	?>
